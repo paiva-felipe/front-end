455880032084add7c5ad43982ac4968791a4241a
paiva-felipe <felipe.g.paiva7@aluno.senai.br>
2026-02-26 15:49:18 -0300

    Codigs JS no arquivo script junto aos codigos HTML e CSS

--- aula3/exercicios_aula4/ex_script.js
+++ aula3/exercicios_aula4/ex_script.js
@@ -4,5 +4,18 @@ const sobrenome = prompt("digite seu sobrenome")
 let nomeFormatado = nome.trim().toLowerCase() + sobrenome.trim().toLowerCase() 
 alert(`${nomeFormatado}`)
 
-let numeroNome = nome.length()+ sobrenome.length()
-alert(`${numeroNome}`)
+alert(`${nomeFormatado.length}`)
+
+///////////// exc 2
+
+const valorTotal= prompt("Qual é o valor total da conta?")
+const quantidadePessoas = prompt("Qual a quantidade de pessoas?")
+const pagar = valorTotal/ quantidadePessoas
+alert(`cada um deve pagar: ${pagar.toFixed(2)}`)
+
+/////// exc 3
+const ValorCoempra = prompt("Qual o valor da compra")
+const cupom = prompt("Você tem cupom?")
+if(cupom = "sim"){
+    alert
+}
